4094acfabc5c40dd907f560ca3bf6b24b155f4a5
JKaterina <uljanova.jekaterina@gmail.com>
2023-12-06 08:49:01 +0000

    puzzle 6, both parts

--- 6.py
+++ 6.py
@@ -32,3 +32,21 @@ for pair in pairs:
 
 print("Answer to part 1: ", result)
 
+### Part 2 ###
+
+# get the new time
+new_time = data[0].split(': ')[1].split(' ')
+final_time = ""
+for i in new_time:
+    final_time += i
+final_time = int(final_time)
+
+# get the new distance
+new_distance = data[1].split(': ')[1].split(' ')
+final_distance = ""
+for i in new_distance:
+    final_distance += i
+final_distance = int(final_distance)
+
+# reuse the function from part 1
+print("Answer to part 2: ", get_distance_travelled((final_time, final_distance)))
